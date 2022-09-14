ea4b0bf4c03a99dd94df04c7824fe79968966c2b
YJK98 <112464350+YJK98@users.noreply.github.com>
2022-09-14 10:02:27 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,50 +33,56 @@ export default function Home() {
         <header style={{position: 'fixed', top: 0, background: '#232323', width: '100%', padding: '5px'}}><p> 🤒 Ref Temp: <Input type="number" step="0.1" className={styles.inputN} value={refT} onChange={e => setRefT(e.target.value)} />⁰C</p></header>
 <br/><br/>
         <p className={styles.title}>
-          mV ↔ ⁰C
+          mV ↔ ℃
         </p>
 
         <div className={styles.grid}>
           <div className={styles.card}>
             <div style={{ background: '#F2F4F6', color: '#232323' }}><b>J</b></div>
-            <p>-190 ⁰C: {TtomVWithRef('j', refT, -190)} mV</p>
-            <p>600 ⁰C: {TtomVWithRef('j', refT, 600)} mV</p>
-            <p>1150 ⁰C: {TtomVWithRef('j', refT, 1150)} mV</p>
+            <p>-190 ℃: {TtomVWithRef('j', refT, -190)} mV</p>
+            <p>600 ℃: {TtomVWithRef('j', refT, 600)} mV</p>
+            <p>1150 ℃: {TtomVWithRef('j', refT, 1150)} mV</p>
 
             <div style={{ background: '#F2F4F6', color: '#232323' }}><b>K</b></div>
-            <p>-190 ⁰C: {TtomVWithRef('k', refT, -190)} mV</p>
-            <p>700 ⁰C: {TtomVWithRef('k', refT, 700)} mV</p>
-            <p>1350 ⁰C: {TtomVWithRef('k', refT, 1350)} mV</p>
+            <p>-190 ℃: {TtomVWithRef('k', refT, -190)} mV</p>
+            <p>700 ℃: {TtomVWithRef('k', refT, 700)} mV</p>
+            <p>1350 ℃: {TtomVWithRef('k', refT, 1350)} mV</p>
 
             <div style={{ background: '#F2F4F6', color: '#232323' }}><b>T</b></div>
-            <p>-190 ⁰C: {TtomVWithRef('t', refT, -190)} mV</p>
-            <p>5 ⁰C: {TtomVWithRef('t', refT, 5)} mV</p>
-            <p>360 ⁰C: {TtomVWithRef('t', refT, 360)} mV</p>
+            <p>-190 ℃: {TtomVWithRef('t', refT, -190)} mV</p>
+            <p>5 ℃: {TtomVWithRef('t', refT, 5)} mV</p>
+            <p>360 ℃: {TtomVWithRef('t', refT, 360)} mV</p>
 
             <div style={{ background: '#F2F4F6', color: '#232323' }}><b>E</b></div>
-            <p>-190 ⁰C: {TtomVWithRef('e', refT, -190)} mV</p>
-            <p>500 ⁰C: {TtomVWithRef('e', refT, 500)} mV</p>
-            <p>950 ⁰C: {TtomVWithRef('e', refT, 950)} mV</p>
+            <p>-190 ℃: {TtomVWithRef('e', refT, -190)} mV</p>
+            <p>500 ℃: {TtomVWithRef('e', refT, 500)} mV</p>
+            <p>950 ℃: {TtomVWithRef('e', refT, 950)} mV</p>
 
             <div style={{ background: '#F2F4F6', color: '#232323' }}><b>R</b></div>
-            <p>5 ⁰C: {TtomVWithRef('r', refT, 5)} mV</p>
-            <p>800 ⁰C: {TtomVWithRef('r', refT, 800)} mV</p>
-            <p>1700 ⁰C: {TtomVWithRef('r', refT, 1700)} mV</p>
+            <p>5 ℃: {TtomVWithRef('r', refT, 5)} mV</p>
+            <p>800 ℃: {TtomVWithRef('r', refT, 800)} mV</p>
+            <p>1700 ℃: {TtomVWithRef('r', refT, 1700)} mV</p>
 
             <div style={{ background: '#F2F4F6', color: '#232323' }}><b>S</b></div>
-            <p>5 ⁰C: {TtomVWithRef('s', refT, 5)} mV</p>
-            <p>800 ⁰C: {TtomVWithRef('s', refT, 800)} mV</p>
-            <p>1700 ⁰C: {TtomVWithRef('s', refT, 1700)} mV</p>
+            <p>5 ℃: {TtomVWithRef('s', refT, 5)} mV</p>
+            <p>800 ℃: {TtomVWithRef('s', refT, 800)} mV</p>
+            <p>1700 ℃: {TtomVWithRef('s', refT, 1700)} mV</p>
 
             <div style={{ background: '#F2F4F6', color: '#232323' }}><b>B</b></div>
-            <p>600 ⁰C: {TtomVWithRef('b', refT, 600)} mV</p>
-            <p>1800 ⁰C: {TtomVWithRef('b', refT, 1800)} mV</p>
+            <p>600 ℃: {TtomVWithRef('b', refT, 600)} mV</p>
+            <p>1800 ℃: {TtomVWithRef('b', refT, 1800)} mV</p>
             {/* <p> ⁰C: {TtomVWithRef('b', refT, )} mV</p> */}
 
             <div style={{ background: '#F2F4F6', color: '#232323' }}><b>N</b></div>
-            <p>5 ⁰C: {TtomVWithRef('n', refT, 5)} mV</p>
-            <p>700 ⁰C: {TtomVWithRef('n', refT, 700)} mV</p>
-            <p>1300 ⁰C: {TtomVWithRef('n', refT, 1300)} mV</p>
+            <p>5 ℃: {TtomVWithRef('n', refT, 5)} mV</p>
+            <p>700 ℃: {TtomVWithRef('n', refT, 700)} mV</p>
+            <p>1300 ℃: {TtomVWithRef('n', refT, 1300)} mV</p>
+          </div>
+          <div className={styles.card}>
+            <h2>Fluke True RMS MM</h2>
+            <div style={{ background: '#F2F4F6', color: '#232323' }}><b>K</b></div>
+            <p>50 ℃: {TtomVWithRef('k', refT, 50)} mV</p>
+            <p>350 ℃: {TtomVWithRef('k', refT, 350)} mV</p>
           </div>
           <div className={styles.card}>
             <h2><Input className={styles.inputN} step="0.1" type="number" value={TCC} onChange={e => setTCC(e.target.value)} />⁰C (ref T) &rarr;</h2>
@@ -94,18 +100,18 @@ export default function Home() {
         <div className={styles.grid}>
           <div className={styles.card}>
             <h2><Input className={styles.inputN} type="number" value={TCmV} onChange={e => setTCmV(e.target.value)} />mV &rarr;</h2>
-            <p><b>B: </b> {Thermocouple.millivoltsToTemp('b', Number(TCmV))} ⁰C</p>
-            <p><b>E: </b> {Thermocouple.millivoltsToTemp('e', Number(TCmV))} ⁰C</p>
-            <p><b>J: </b> {Thermocouple.millivoltsToTemp('j', Number(TCmV))} ⁰C</p>
-            <p><b>K: </b> {Thermocouple.millivoltsToTemp('k', Number(TCmV))} ⁰C</p>
-            <p><b>N: </b> {Thermocouple.millivoltsToTemp('n', Number(TCmV))} ⁰C</p>
-            <p><b>R: </b> {Thermocouple.millivoltsToTemp('r', Number(TCmV))} ⁰C</p>
-            <p><b>S: </b> {Thermocouple.millivoltsToTemp('s', Number(TCmV))} ⁰C</p>
-            <p><b>T: </b> {Thermocouple.millivoltsToTemp('t', Number(TCmV))} ⁰C</p>
+            <p><b>B: </b> {Thermocouple.millivoltsToTemp('b', Number(TCmV))} ℃</p>
+            <p><b>E: </b> {Thermocouple.millivoltsToTemp('e', Number(TCmV))} ℃</p>
+            <p><b>J: </b> {Thermocouple.millivoltsToTemp('j', Number(TCmV))} ℃</p>
+            <p><b>K: </b> {Thermocouple.millivoltsToTemp('k', Number(TCmV))} ℃</p>
+            <p><b>N: </b> {Thermocouple.millivoltsToTemp('n', Number(TCmV))} ℃</p>
+            <p><b>R: </b> {Thermocouple.millivoltsToTemp('r', Number(TCmV))} ℃</p>
+            <p><b>S: </b> {Thermocouple.millivoltsToTemp('s', Number(TCmV))} ℃</p>
+            <p><b>T: </b> {Thermocouple.millivoltsToTemp('t', Number(TCmV))} ℃</p>
           </div>
 
           <div className={styles.card}>
-            <h2><Input className={styles.inputN} step="0.1" type="number" value={TCC} onChange={e => setTCC(e.target.value)} />⁰C &rarr;</h2>
+            <h2><Input className={styles.inputN} step="0.1" type="number" value={TCC} onChange={e => setTCC(e.target.value)} />℃ &rarr;</h2>
             <p><b>B: </b> {Thermocouple.tempToMillivolts('b', Number(TCC))} mV</p>
             <p><b>E: </b> {Thermocouple.tempToMillivolts('e', Number(TCC))} mV</p>
             <p><b>J: </b> {Thermocouple.tempToMillivolts('j', Number(TCC))} mV</p>
